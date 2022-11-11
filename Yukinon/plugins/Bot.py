2bb30092d82a4123135562416242ec8eba93625a
Jhhuuuufc <117942420+Ttqqk@users.noreply.github.com>
2022-11-11 20:11:35 +0200

    Update Bot.py

--- Yukinon/plugins/Bot.py
+++ Yukinon/plugins/Bot.py
@@ -1,1 +1,12 @@
-0
+import asyncio
+from pyrogram.types import Message
+from Yukinon import app
+from config import OWNER_ID
+from Yukinon.utils.custom_filters import restrict_filter
+from Yukinon.utils.commands import *
+
+@app.on_message(
+ command(["بوت","البوت"]) & filters.user(OWNER_ID)
+)
+async def bot(client: Client, message: Message):
+    await message.reply_text(f"◍ نعم حبيبى المطور 🥺❤️\n√")
