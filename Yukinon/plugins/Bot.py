2bf58ccc3e6fb521cd55d5a01882a31fce7d5cfe
Jhhuuuufc <117942420+Ttqqk@users.noreply.github.com>
2022-11-11 20:38:16 +0200

    Update Bot.py

--- Yukinon/plugins/Bot.py
+++ Yukinon/plugins/Bot.py
@@ -11,3 +11,6 @@ from Yukinon.utils.commands import *
 )
 async def bot(client: Client, message: Message):
     await message.reply_text(f"◍ نعم حبيبى المطور 🥺❤️\n√")
+else:
+async def bot(client: Client, message: Message):
+    await message.reply_text(f"◍ اسمى شادو ياحب 🙄❤️\n√")
