bdc8ff5547cc50bc44d973f71ad49c268fd4bdc8
Jhhuuuufc <117942420+Ttqqk@users.noreply.github.com>
2022-11-11 15:42:11 +0200

    Create Rboys.py

---
+++ Yukinon/Games/Rboys.py
@@ -0,0 +1,6 @@
+IMG = ["https://telegra.ph/file/c8f5c1dd990ca9a3d8516.jpg",
+       "https://telegra.ph/file/77cc3154b752ce822fd52.jpg",
+       "https://telegra.ph/file/e72fb0b6a7fba177cf4c7.jpg",
+       "https://telegra.ph/file/8738a478904238e367939.jpg",
+       "https://telegra.ph/file/68d7830ba72820f44bda0.jpg"
+]
