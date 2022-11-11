9f7449995ab30ed205747b75924b0a5c938b459c
Jhhuuuufc <117942420+Ttqqk@users.noreply.github.com>
2022-11-11 21:05:33 +0200

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,6 @@ SUDO_USERS_ID = environ.get("SUDO_USERS_ID")
 LOG_GROUP_ID = environ.get("LOG_GROUP_ID","-1001569455288")
 MONGO_URL = environ.get("MONGO_URL","mongodb+srv://vampir:Al2552001@cluster0.yeaoc.mongodb.net/vampir?retryWrites=true&w=majority")
 BASE_DB = MONGO_URL
-BANNED_USERS = filters.user()
 ARQ_API_URL = environ.get("ARQ_API_URL", "https://arq.hamker.in")
 ARQ_API_KEY = environ.get("ARQ_API_KEY","PLBNYD-CGIRFG-RECXWF-UFZIOA-ARQ")
 F_SUB_CHANNEL = environ.get("F_SUB_CHANNEL", "FA9SH")
