5c90bf489b35df3ee83f80f6b6cbad52b331c956
izoky <87774071+izoky@users.noreply.github.com>
2022-11-12 13:16:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ BOT_TOKEN = environ.get("BOT_TOKEN")
 API_ID = int(environ.get("API_ID","7634570"))
 API_HASH = environ.get("API_HASH","49265e23e8cb8218ac89d60777f280a6")
 SUDO_USERS_ID = environ.get("SUDO_USERS_ID")
-LOG_GROUP_ID = environ.get("LOG_GROUP_ID","-1001569455288")
+LOG_GROUP_ID = environ.get("LOG_GROUP_ID","-1001534549293")
 MONGO_URL = environ.get("MONGO_URL","mongodb+srv://vampir:Al2552001@cluster0.yeaoc.mongodb.net/vampir?retryWrites=true&w=majority")
 BASE_DB = MONGO_URL
 ARQ_API_URL = environ.get("ARQ_API_URL", "https://arq.hamker.in")
